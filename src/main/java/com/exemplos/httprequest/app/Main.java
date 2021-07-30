dea6c90e88d5c010a514f892c767b3349cb3b0ee
Guto <augustosouzacampos99@gmail.com>
2021-07-30 12:52:39 -0300

    Implementação

--- src/main/java/com/exemplos/httprequest/app/Main.java
+++ src/main/java/com/exemplos/httprequest/app/Main.java
@@ -6,8 +6,10 @@ import java.nio.charset.StandardCharsets;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
@@ -22,6 +24,8 @@ public class Main {
 		try {
 			httpGet();
 			httpPost();
+			httpPut();
+			httpDelete();
 		} 
 		catch (Exception e) {
 			e.printStackTrace();
@@ -59,6 +63,39 @@ public class Main {
 		System.out.println(response.getStatusLine().getStatusCode());
 		System.out.println(responseBody);
 		System.out.println(responseDto.getJson().toString());
+	}
+
+	private static void httpPut() throws Exception {
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpPut request = new HttpPut("https://httpbin.org/put");
+		Gson gson = new Gson();
+		
+		request.setHeader("Content-type", "application/json");
+		User user = new User("admin","123456");
+		
+		StringEntity stringEntity = new StringEntity(gson.toJson(user));
+		request.setEntity(stringEntity);
+		
+		HttpResponse response = client.execute(request);
+		String responseBody = EntityUtils.toString(response.getEntity(), "UTF-8");
+		ResponseDTO responseDto = gson.fromJson(responseBody, ResponseDTO.class);
+
+		System.out.println(response.getStatusLine().getStatusCode());
+		System.out.println(responseBody);
+		System.out.println(responseDto.getJson().toString());
+	}
+
+	private static void httpDelete() throws Exception {
+		HttpClient client = HttpClientBuilder.create().build();
+		HttpDelete request = new HttpDelete("https://httpbin.org/delete");
+		
+		request.setHeader("Content-type", "application/json");		
+
+		HttpResponse response = client.execute(request);
+		String responseBody = EntityUtils.toString(response.getEntity(), "UTF-8");
+
+		System.out.println(response.getStatusLine().getStatusCode());
+		System.out.println(responseBody);
 	}
 
 }
